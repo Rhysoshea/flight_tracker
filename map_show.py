de44e10b944e30f260a792590e80fbc56afa43bf
rhys <rhys.shea@gmail.com>
2020-05-09 18:00:12 +0900

    fixed error with displaying map, had to correct code in cartopy library

--- map_show.py
+++ map_show.py
@@ -1,25 +1,36 @@
 import urllib.request
 import json
+from matplotlib.path import Path
+
 import matplotlib.pyplot as plt
 from matplotlib import animation
 import cartopy.crs as ccrs
 from cartopy.io.img_tiles import OSM
 
 
-#define figure
-fig, ax = plt.subplots()
+##ax = plt.axes(projection=ccrs.Mollweide())
+##ax.stock_img()
+##plt.show()
 
-#set axis for plotting area
-ax = plt.axes(projection=ccrs.PlateCarree())
-ax.set_ylim(40.6051, 40.6825)
-ax.set_xlim(-73.8288,-73.7258)
+#define figure
+##fig, ax = plt.subplots()
+
+
 
 #add osm basemap
+
 osm_tiles=OSM()
-ax.add_image(osm_tiles,13)#zoom level 13
+ax = plt.axes(projection=osm_tiles.crs)
+ax.set_extent((-73.76, -73.80, 40.63, 40.65))
+#set axis for plotting area
+##ax.set_ylim(40.6051, 40.6825)
+##ax.set_xlim(-73.8288,-73.7258)
+
+ax.add_image(osm_tiles,15)#zoom level 13
 
 #plot jfk intl airport
 ax.text(-73.778889, 40.639722,'JFK Intl', horizontalalignment='right',size='large')
 ax.plot([-73.778889],[40.639722],'bo')#plot blue point
 
 plt.show()
+
